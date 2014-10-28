31344b099d40ff21a94fe8ca6b856dea6f6a5653
lostbean <talktoedgar@gmail.com>
2014-10-28 23:02:23 +0100

    add ODF sampling with hit&run / slice monte carlo markov chain algorithm.

---
+++ profile/TestKernelSampling.hs
@@ -0,0 +1,48 @@
+module TestKernelSampling where
+
+import qualified Data.Vector.Unboxed as U
+
+import Hammer.Math.Algebra
+import Hammer.VTK
+
+import qualified Data.BlazeVPtree as VP
+
+import Texture.Orientation
+import Texture.Symmetry
+import Texture.IsoSphere
+import Texture.TesseractGrid
+import Texture.Kernel
+import Texture.Sampler
+
+instance VP.Metric Quaternion where
+  dist = getMisoAngle Cubic
+
+-- | Test VP tree on Rodrigues-Frank space
+testKernelSampling :: Int -> IO ()
+testKernelSampling n = do
+  -- Initial ODF
+  let
+    vs = U.filter (isInRodriFZ Cubic) $ genQuaternionGrid 80
+    vp = VP.fromVector vs
+
+  let
+    qs1 = U.map (toQuaternion . mkAxisPair (Vec3 1 1 1) . Deg) $ U.enumFromStepN (-45) 2 45
+    qs2 = U.map (toQuaternion . mkAxisPair (Vec3 (-1) (-1) 1) . Deg) $ U.enumFromStepN (-45) 2 45
+    qs = qs1 U.++ qs2
+    ks = addManyKernels (Deg 3) vp qs (U.replicate (U.length vs) 0)
+
+  renderODF (vs, ks) "/home/edgar/InputODF"
+
+  -- Sampling ODF
+  let pdf x = maybe 0 (\((i,_,_)) -> ks U.! i) (VP.nearestThanNeighbor vp (4/80) x)
+  xs <- hitAndRunSlice defaultCfg pdf zerorot 40000
+  let ts = addManyKernels (Deg 1.5) vp (U.fromList xs) (U.replicate (U.length vs) 0)
+
+  renderODF (vs, ts) "/home/edgar/OutputODF"
+
+-- | Render ODF
+renderODF :: (U.Vector Quaternion, U.Vector Double) -> String -> IO ()
+renderODF (qs, vs) file = let
+  attr = mkPointValueAttr "Intensity" (\i _ -> vs U.! i)
+  vtk  = renderQuaternions qs []
+  in writeUniVTKfile (file ++ ".vtu") True (addPointValueAttr vtk attr)
