ee15bd4329a82f4666fb2159fcd054cce3cad73f
lostbean <talktoedgar@gmail.com>
2014-11-14 18:09:29 +0100

    add CTF file reader

---
+++ src/File/CTFReader.hs
@@ -0,0 +1,256 @@
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Read and load *.ctf files from EBSD measure systems.
+module File.CTFReader
+       ( parseCTF
+       , ebsdToVoxBox
+       , EBSDpoint (..)
+       , EBSDinfo  (..)
+       , EBSDphase (..)
+       , Gridinfo  (..)
+       , EBSDdata  (..)
+       ) where
+
+import qualified Data.Vector                      as V
+import qualified Data.Vector.Unboxed              as U
+import qualified Data.ByteString                  as B
+import qualified Data.ByteString.Char8            as BC
+import qualified Data.Attoparsec.ByteString.Char8 as AC
+
+import           Hammer.VoxBox
+
+import           Texture.Orientation (Quaternion, toQuaternion, mkEuler, Deg(..))
+import           Data.Vector         (Vector)
+import           Control.Applicative ((<|>), (<$>))
+
+import           Data.Attoparsec.ByteString.Char8
+
+-- ============================== CTF manipulation =======================================
+
+ebsdToVoxBox :: (U.Unbox a)=> EBSDdata -> (EBSDpoint -> a) -> VoxBox a
+ebsdToVoxBox EBSDdata{..} func = box
+  where
+    EBSDinfo{..}   = ebsdInfo
+    Gridinfo{..}   = grid
+    (xstep, ystep) = xystep
+    (row, col)     = rowsCols
+    boxorg = VoxelPos 0 0 0
+    boxdim = VoxBoxDim col row 1
+    dime   = VoxBoxRange boxorg boxdim
+    org    = VoxBoxOrigin 0 0 0
+    spc    = VoxelDim xstep ystep ((xstep + ystep) / 2)
+    box    = VoxBox dime org spc (V.convert $ V.map func nodes)
+
+-- ============================== Data definition ========================================
+
+-- | Information associated to each point.
+-- Relation with OIM parameters:
+-- CI  <-> (number of bands / max. number of bands)* or (error / max. error)
+-- IQ  <-> band contrast* or band slope
+-- fit <-> mad (mean angular deviation)
+-- 
+-- OBS. For hexagonal grid a rotation of 30 deg (phi 2) may be necessary. The standard
+-- options are marked with (*).
+data EBSDpoint
+  = EBSDpoint
+  { phase        :: {-# UNPACK #-} !Int     -- ^ phase id (0 means non-indexed)
+  , rotation     :: {-# UNPACK #-} !Quaternion
+  , xpos         :: {-# UNPACK #-} !Int     -- ^ x position in um
+  , ypos         :: {-# UNPACK #-} !Int     -- ^ y position in um
+  , bands        :: {-# UNPACK #-} !Int     -- ^ number of bands found
+  , errorID      :: {-# UNPACK #-} !Int     -- ^ error
+  , angulardev   :: {-# UNPACK #-} !Double  -- ^ MAD (mean angular deviation)
+  , bandcontrast :: {-# UNPACK #-} !Int     -- ^ BC  (band contrast)
+  , bandslope    :: {-# UNPACK #-} !Int     -- ^ BS  (band slope)
+  } deriving Show
+
+-- | Information describing the measuriment.
+data EBSDinfo =
+  EBSDinfo
+  { project :: String
+  , jobMode :: String
+  , author  :: String
+  , info    :: [String]
+  } deriving (Show)
+
+data EBSDphase =
+  EBSDphase
+  { phaseID      :: Int
+  , latticeCons  :: (Double, Double, Double, Double, Double, Double)
+  , materialName :: String
+  , phaseInfo    :: [String]
+  } deriving Show
+
+-- | Information about the grid of point. Hexagonal or Square
+data Gridinfo
+  = Gridinfo
+  { rowsCols :: (Int, Int)
+  , xystep   :: (Double, Double)
+  , origin   :: (Double, Double, Double)
+  } deriving Show
+
+-- | Hold the whole CTF data strcuture
+data EBSDdata
+  = EBSDdata
+  { nodes    :: Vector EBSDpoint
+  , grid     :: Gridinfo
+  , ebsdInfo :: EBSDinfo
+  , phases   :: [EBSDphase]
+  } deriving Show
+
+-- ============================== Parse functions ========================================
+
+-- | Read the input CTF file. Rise an error mesage in case of bad format or acess.
+parseCTF :: String -> IO EBSDdata
+parseCTF fileName = do
+  stream <- B.readFile fileName
+  let
+    ebsd = case parseOnly parseEBSDdata stream of
+      Left err -> error ("[CTF] Error reading file " ++ fileName ++ "\n" ++ show err)
+      Right x  -> x
+  checkDataSize  ebsd
+  return ebsd
+
+parseEBSDdata :: Parser EBSDdata
+parseEBSDdata = do
+  string "Channel Text File" >> eol
+  _proj   <- getInfo "Prj"      parseText
+  _auth   <- getInfo "Author"   parseText
+  _job    <- getInfo "JobMode"  parseText
+  _grid   <- gridParse
+  _info   <- parseFields
+  _phases <- many1' phaseParse
+  skipWhile (not . isEOL) >> eol
+  _points <- many1' (pointParse _grid)
+  let _head = EBSDinfo _proj _job _auth _info
+  return $ EBSDdata (V.fromList _points) _grid _head _phases
+
+checkDataSize :: EBSDdata -> IO ()
+checkDataSize EBSDdata{..}
+  | V.length nodes == n = putStrLn $ "[CTF] Loaded numbers of points: " ++ show n
+  | otherwise = msg
+  where
+    Gridinfo{..} = grid
+    (row, col)   = rowsCols
+    n = row * col
+    msg = error $ "[CTF] Invalid numbers of points. Expected " ++
+          show n ++ " but found " ++
+          show (V.length nodes) ++ ". The grid shape is given by (row, cEven, cOdd) = " ++
+          show rowsCols
+
+-- ------------------------------------ SubParsers ---------------------------------------
+
+phaseParse :: Parser EBSDphase
+phaseParse = do
+  phn <- getInfo "Phases" parseInt
+  eol
+  lat <- latticeParse
+  mat <- parseField
+  rs  <- parseFields
+  return $ EBSDphase phn lat mat rs
+
+gridParse :: Parser Gridinfo
+gridParse = do
+  row   <- getInfo "XCells" parseInt
+  col   <- getInfo "YCells" parseInt
+  xstep <- getInfo "XStep"  parseFloat
+  ystep <- getInfo "YStep"  parseFloat
+  x     <- getInfo "AcqE1"  parseFloat
+  y     <- getInfo "AcqE2"  parseFloat
+  z     <- getInfo "AcqE3"  parseFloat
+  return $ Gridinfo (row, col) (xstep, ystep) (x, y, z)
+
+latticeParse :: Parser (Double, Double, Double, Double, Double, Double)
+latticeParse = do
+  a  <- parseFloat
+  string ";"
+  b  <- parseFloat
+  string ";"
+  c  <- parseFloat
+  blanks
+  w1 <- parseFloat
+  string ";"
+  w2 <- parseFloat
+  string ";"
+  w3 <- parseFloat
+  eol
+  return (a, b, c, w1, w2, w3)
+
+pointParse :: Gridinfo -> Parser EBSDpoint
+pointParse g = do
+  ph   <- parseInt
+  x    <- parseFloat
+  y    <- parseFloat
+  bd   <- parseInt
+  er   <- parseInt
+  phi1 <- parseFloat
+  phi  <- parseFloat
+  phi2 <- parseFloat
+  ma   <- parseFloat
+  bc   <- parseInt
+  bs   <- parseInt
+  eol
+  let rot     = toQuaternion $ mkEuler (Deg phi1) (Deg phi) (Deg phi2)
+      (xi,yi) = getGridPoint g (x, y)
+  return $ EBSDpoint ph rot xi yi bd er ma bc bs
+
+-- | Calculate colunm position (row,col) from ID sequence
+-- Origin at (1,1)
+getGridPoint :: Gridinfo -> (Double, Double) -> (Int, Int)
+getGridPoint g (x, y) = let
+  (xstep, ystep) = xystep g
+  yi = round ((2*y)/ystep) `div` 2
+  -- divide for 0.5*xstep to avoid error when round value
+  -- ex. round 7.4/5.0 \= round 7.6/5.0;
+  xi = round ((2*x)/xstep) `div` 2
+  in (xi, yi)
+
+getInfo :: B.ByteString -> Parser a -> Parser a
+getInfo ident func = let
+  parser = do
+    string ident
+    x <- func
+    eol
+    return x
+  in parser <|> do
+    found <- BC.unpack <$> AC.take (B.length ident)
+    fail ("Can't find the following field -> " ++ BC.unpack ident ++ ", found -> " ++ found )
+
+-- -------------------------------------- Basic parsers ----------------------------------
+
+parseFields :: Parser [String]
+parseFields = do
+  x <- manyTill' parseField (string "\r" <|> string "\n")
+  eol
+  return x
+
+parseField :: Parser String
+parseField = blanks >> (BC.unpack <$> AC.takeWhile (\c -> not (isEOL c) && not (isEOField c)))
+
+parseText :: Parser String
+parseText = blanks >> ((unwords . words . BC.unpack) <$> AC.takeWhile (not . isEOL))
+
+parseFloat :: Parser Double
+parseFloat = blanks >> signed double
+
+parseInt :: Parser Int
+parseInt = blanks >> signed decimal
+
+-- | Skips blank chars (space and tab)
+blanks :: Parser ()
+blanks = skipWhile isBlank
+
+isEOField :: Char -> Bool
+isEOField = (== '\t')
+
+isBlank :: Char -> Bool
+isBlank c = c == ' ' || c == '\t'
+
+isEOL :: Char -> Bool
+isEOL c = c == '\r' || c == '\n'
+
+-- | Skips blanks chars till and including the eol (End Of Line - CR-LF or LF)
+eol :: Parser ()
+eol = blanks >> skipWhile isEOL
